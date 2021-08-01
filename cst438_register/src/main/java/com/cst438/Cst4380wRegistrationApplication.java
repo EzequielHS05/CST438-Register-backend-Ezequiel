c41c1125d59f220c3aee685a5f0e7b2e16d1f93b
David <dwisneski@csumb.edu>
2021-08-01 08:27:46 -0700

    Remove basic security and add @CrossOrigin to Rest Controller.

--- cst438_register/src/main/java/com/cst438/Cst4380wRegistrationApplication.java
+++ cst438_register/src/main/java/com/cst438/Cst4380wRegistrationApplication.java
@@ -1,23 +1,15 @@
 package com.cst438;
 
-import java.util.Arrays;
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.context.annotation.Bean;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.web.cors.CorsConfiguration;
-import org.springframework.web.cors.CorsConfigurationSource;
-import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
-
 import com.cst438.service.GradebookService;
 import com.cst438.service.GradebookServiceMQ;
 import com.cst438.service.GradebookServiceREST;
 
 @SpringBootApplication
-public class Cst4380wRegistrationApplication extends WebSecurityConfigurerAdapter {
+public class Cst4380wRegistrationApplication  {  
 
 	public static void main(String[] args) {
 		SpringApplication.run(Cst4380wRegistrationApplication.class, args);
@@ -41,26 +33,6 @@ public class Cst4380wRegistrationApplication extends WebSecurityConfigurerAdapte
 	@ConditionalOnProperty(prefix="gradebook", name="service", havingValue = "default")
 	public GradebookService gradebookDefault() {
 		return new GradebookService();
-	}
-	
-	@Override
-   	protected void configure(HttpSecurity http) throws Exception {
-		http.csrf().disable().cors();
- 		http.authorizeRequests().antMatchers("/**").permitAll();
-	}
-	
-	// this is necessary to allow cross-origin http requests from React front end. 
-	@Bean
-	CorsConfigurationSource corsConfigurationSource() {
-		UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-		CorsConfiguration config = new CorsConfiguration();
-		config.setAllowedOrigins(Arrays.asList("http://localhost:3000", "https://accounts.google.com"));
-		config.setAllowedMethods(Arrays.asList("*"));
-		config.setAllowedHeaders(Arrays.asList("*"));
-		config.setAllowCredentials(true);
-		config.applyPermitDefaultValues();
-		source.registerCorsConfiguration("/**", config);
-		return source;
 	}
 
 }
