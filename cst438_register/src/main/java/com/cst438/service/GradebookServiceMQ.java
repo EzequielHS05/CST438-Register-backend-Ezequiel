185706371d10960b6446fb2d01d4557e0e715238
David Wisneski <dwisneski@csumb.edu>
2022-09-22 19:54:40 -0700

    Update GradebookServiceMQ.java

--- cst438_register/src/main/java/com/cst438/service/GradebookServiceMQ.java
+++ cst438_register/src/main/java/com/cst438/service/GradebookServiceMQ.java
@@ -33,9 +33,8 @@ public class GradebookServiceMQ extends GradebookService {
 	@Override
 	public void enrollStudent(String student_email, String student_name, int course_id) {
 		 
-		EnrollmentDTO enrollmentDTO = new EnrollmentDTO(student_email, student_name, course_id);
-		
-		rabbitTemplate.convertAndSend(gradebookQueue.getName(), enrollmentDTO);
+		// TODO 
+		// create EnrollmentDTO and send to gradebookQueue
 		
 		System.out.println("Message send to gradbook service for student "+ student_email +" " + course_id);  
 		
@@ -45,13 +44,8 @@ public class GradebookServiceMQ extends GradebookService {
 	public void receive(CourseDTOG courseDTOG) {
 		System.out.println("Receive enrollment :" + courseDTOG);
 
-		// process the list of student grades
-		for (CourseDTOG.GradeDTO g : courseDTOG.grades) {
-			Enrollment e = enrollmentRepository.findByEmailAndCourseId(g.student_email, courseDTOG.course_id);
-			e.setCourseGrade(g.grade);
-			enrollmentRepository.save(e);
-			System.out.println("final grade update " + g.student_email + " " + courseDTOG.course_id + " " + g.grade);
-		}
+		//TODO 
+		// for each student grade in courseDTOG,  find the student enrollment entity, update the grade and save back to enrollmentRepository.
 	}
 
 }
