41d53e2d9248cc67366c93d7ab906e99bff7d80a
EzequielHS05 <ehernandezsimon@outlook.com>
2022-09-26 14:14:58 -0700

    add Student Controller

---
+++ cst438_register/src/main/java/com/cst438/controller/StudentController.java
@@ -0,0 +1,12 @@
+package com.cst438.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class StudentController {
+	
+	
+	public String newStudent() {
+		return "";
+	}
+}
